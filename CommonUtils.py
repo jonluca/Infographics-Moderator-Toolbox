cb9fcc832353c23bc938ca323f6b57893b95fa28
larperdoodle <larsbenedetto@gmail.com>
2017-06-17 03:50:54 -0700

    Added getting settings from file

--- CommonUtils.py
+++ CommonUtils.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+
 import praw
 from datetime import timedelta
 
@@ -10,9 +12,30 @@ def hours_since(timestamp):
     return round(diff.total_seconds() / 3600)
 
 
+def get_settings(filename):
+    settings = []
+    try:
+        if not os.path.exists(filename):
+            raise IOError
+        with open(filename) as file:
+            for line in file:
+                line = str(line.strip())
+                settings.append(line)
+        try:
+            file.close()
+        except:
+            pass
+    except IOError:
+        pass
+    except:
+        pass
+    return settings
+
+
 def get_reddit_instance():
-    return praw.Reddit(client_id='',
-                       client_secret='',
-                       username='',
-                       user_agent='',
-                       password='')
+    settings = get_settings("settings.txt")
+    return praw.Reddit(client_id=settings[0],
+                       client_secret=settings[1],
+                       username=settings[2],
+                       user_agent=settings[3],
+                       password=settings[4])
