bc9538006d298eab63e2fff3624c59bded3caa8a
larperdoodle <larsbenedetto@gmail.com>
2017-06-17 04:19:09 -0700

    Removed array access typo

--- ClearModmail.py
+++ ClearModmail.py
@@ -6,7 +6,7 @@ subreddit = reddit.subreddit('/r/dankmemes')
 while True:
 	for conversation in subreddit.conversations(state='new'):
 		if len(conversation.messages) == 1:
-			for message in conversation.messages[0]:
+			for message in conversation.messages:
 				if message.startswith("Thank you for submitting to"):
 					conversation.archive()
 					time.sleep(2)
