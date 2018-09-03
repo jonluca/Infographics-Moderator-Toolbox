50544215b9decfdcb0e6ebb47a07f4f124a74116
JonLuca DeCaro <jdecaro@usc.edu>
2018-09-03 14:28:37 -0400

    Changed unmoderated to approve all

--- ClearUnmoderated.py
+++ ClearUnmoderated.py
@@ -1,19 +1,9 @@
-import time
-
-from CommonUtils import get_reddit_instance, hours_since
+from CommonUtils import get_reddit_instance
 
 reddit = get_reddit_instance("/r/infographis UnmoderatedClearer v3.0 by /u/JonLuca")
 sub = reddit.subreddit('infographics')
-
-while True:
-    # Remove any post with less than 10 upvotes in the first hour
-    # Approve any post that is more than 24 hours old
-    for submission in sub.mod.unmoderated(limit=None):
-        hours = hours_since(submission.created_utc)
-        if submission.score < 10:
-            if hours > 1:
-                submission.mod.remove()
-        else:
-            if hours > 23:
-                submission.mod.approve()
-        time.sleep(3)
+numApproved = 0
+for submission in sub.mod.unmoderated(limit=None):
+    submission.mod.approve()
+    numApproved += 1
+    print("Approved " + str(numApproved))
